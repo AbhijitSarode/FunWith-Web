6b9255755e458a157c11fb91b12b88e0d7c36f15
Abhijit Sarode <arsarode@hotmail.com>
2021-02-22 08:31:15 +0530

    Project 2 added

---
+++ 2 Roll The Dice/index.js
@@ -0,0 +1,13 @@
+var randonNumber1 = Math.floor((Math.random() * 6) + 1)
+var randonNumber2 = Math.floor((Math.random() * 6) + 1)
+
+document.getElementById('left-img').src = `images/dice${randonNumber1}.png`
+document.querySelector('#right-img').src = `images/dice${randonNumber2}.png`
+
+if (randonNumber1 == randonNumber2) {
+    document.querySelector('h1').innerText = 'Draw!'
+} else if (randonNumber1 < randonNumber2) {
+    document.querySelector('h1').innerText = 'Player 2 won 🚩'
+} else {
+    document.querySelector('h1').innerText = '🚩 Player 1 won'
+}
