4643966309a4ca40ff71db858a430dd330b6d0e6
Abhijit Sarode <arsarode@hotmail.com>
2021-02-25 08:36:30 +0530

    Readme updated

--- 4 The Simon Game/index.js
+++ 4 The Simon Game/index.js
@@ -1,8 +1,18 @@
-// $("body").click(playSound("red"))
+// var buttonColors = ["red", "blue", "green", "yellow"]
+// var gamePattern = []
+
+// function nextPattern() {
+
+//     var randomNumber = Math.floor(Math.random () * 4)
+//     var randomColorChosen = buttonColors[randomNumber]
+//     return gamePattern[randomColorChosen]
+// }
+
+// $("button").click(playSound(nextPattern))
 
 // document.querySelector('.red').addEventListener("click", playSound())
 
-// function playSound() {
-//     var audio = new Audio("sounds/red.mp3")
+// function playSound(sound) {
+//     var audio = new Audio(`sounds/${sound}.mp3`)
 //     audio.play()
 // }
