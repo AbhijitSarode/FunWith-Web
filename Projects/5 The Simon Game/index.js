c6b10dc85a227268796a5aa401adcd901173b900
Abhijit Sarode <arsarode@hotmail.com>
2021-02-28 04:03:12 +0530

    Game completed

--- Projects/5 The Simon Game/index.js
+++ Projects/5 The Simon Game/index.js
@@ -1,18 +1,82 @@
-// var buttonColors = ["red", "blue", "green", "yellow"]
-// var gamePattern = []
+var buttonColors = ["red", "blue", "green", "yellow"]
+var gamePattern = []
+var userClickedPattern = []
+var level = 0
+var started = false
 
-// function nextPattern() {
+$('button').click(function(){
+    var userChosenColor = $(this).attr("class")
+    userClickedPattern.push(userChosenColor)
 
-//     var randomNumber = Math.floor(Math.random () * 4)
-//     var randomColorChosen = buttonColors[randomNumber]
-//     return gamePattern[randomColorChosen]
-// }
+    playSound(userChosenColor)
+    animatePress(userChosenColor)
+    checkAnswer(userClickedPattern.length-1)
+})
 
-// $("button").click(playSound(nextPattern))
+$(document).keypress(function(){
 
-// document.querySelector('.red').addEventListener("click", playSound())
+    if (!started) {
+        var heading = $(this).text()
+        updateHeading(0)
+        nextSequence()
+        started = true
+    }
+})
 
-// function playSound(sound) {
-//     var audio = new Audio(`sounds/${sound}.mp3`)
-//     audio.play()
-// }
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4)
+    var randomChosenColour = buttonColors[randomNumber]
+    gamePattern.push(randomChosenColour)
+
+    animatePress(randomChosenColour)
+    playSound(randomChosenColour)
+
+    level++
+    userClickedPattern = []
+    updateHeading(level)
+}
+
+function playSound(sound) {
+    var audio = new Audio(`sounds/${sound}.mp3`)
+    audio.play()
+}
+
+function animatePress(btnTapped) {
+    // $("." + btnTapped).fadeIn(100).fadeOut(100).fadeIn(100)
+    $("." + btnTapped).addClass('pressed')
+
+    setTimeout(function(){
+        $("." + btnTapped).removeClass('pressed')
+    }, 100)
+}
+
+function updateHeading(levelNumber) {
+    $('h1').text("level " + levelNumber)
+}
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+        console.log('success')
+        if (userClickedPattern.length == gamePattern.length) {
+            setTimeout(function(){
+                nextSequence()
+            },1000)
+        }
+    } else {
+        console.log('failed')
+        playSound("wrong")
+        $('body').addClass('game-over')
+        setInterval(function(){
+            $('body').removeClass("game-over")
+        }, 200);
+        $('h1').text("Game over, Press 'A' to start")
+        startOver()
+    }
+}
+
+function startOver() {
+    started = false
+    level = 0
+    gamePattern.length = 0
+    userClickedPattern = []
+}
