ebeed504b10e9ba6b71a01cbb50d96b481e2f36c
Abhijit Sarode <arsarode@hotmail.com>
2021-03-01 05:23:51 +0530

    Logic added

---
+++ Projects/4 Guess The Color/README.md
@@ -0,0 +1,11 @@
+# FunWith-Web
+This repo is a collection of all the small projects built to learn the web skills
+
+## Project 1: Personal Website
+This project uses HTML/CSS 
+
+## Project 2: Roll The Dice
+This project uses DOM manipulation
+
+## Project 3: Drum Kit
+This project uses DOM manipulation & audio
---
+++ Projects/4 Guess The Color/index.js
@@ -0,0 +1,60 @@
+var colors = generataRandomColors(6)
+
+var squares = document.querySelectorAll('.square')
+var colorDisplay = document.querySelector('#color-display')
+var messgeDisplay = document.querySelector("#message")
+var h1 = document.querySelector('h1')
+// var pickedColor = colors[3]
+var pickedColor = pickedColor()
+colorDisplay.textContent = pickedColor
+
+for(var i = 0; i< squares.length; i++) {
+    //Add intial colors to squares
+    squares[i].style.background = colors[i]
+
+    //Add click listeners to squares
+    squares[i].addEventListener('click', function(){
+        //Grab color of the clicked square
+        var tappedColor = this.style.background
+        //Compare color of the pickeColor
+        if (tappedColor === pickedColor) {
+            messgeDisplay.textContent = "Correct👏"
+            h1.style.backgroundColor = pickedColor
+            changeColors()
+        } else {
+            this.style.background = "rgb(53, 52, 52)"
+            messgeDisplay.textContent = "Try again"
+        }
+    })
+}
+
+function changeColors(){
+    for(var i = 0; i< squares.length; i++) {
+        squares[i].style.background = pickedColor
+    }
+}
+
+function pickedColor() {
+    var random = Math.floor(Math.random() * 6)
+    return colors[random]
+}
+
+function generataRandomColors(number) {
+    // make an array
+    var arr = []
+    //add 'x' number of random colors to array
+    for (let i = 0; i < number; i++) {
+        arr.push(randomColor())
+        
+    }
+    //return the array
+    return arr
+}
+
+function randomColor() {
+    var red = Math.floor(Math.random() * 256)
+    var green = Math.floor(Math.random() * 256)
+    var blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
